b78ccd93a1733f07c5f15a3c30d4c08684031313
Anjali Longre <anjalilongre349@gmail.com>
2025-02-03 23:20:40 +0530

    instagram with ejs

--- EJS/index.js
+++ EJS/index.js
@@ -1,13 +1,36 @@
 const express=require("express");
 const app=express();
+const path=require("path");
 
 const port=8080;
 
-app.set("viewengine","ejs");
-
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
 
 app.get("/",(req,res)=>{
-    res.send("this is root");
+    res.render("home.ejs");
+})
+
+app.get("/ig/:username",(req,res)=>{
+    let {username}=req.params;
+    const instaData=require("./data.json");
+    const data=instaData[username];
+    if(data){
+    res.render("instagram.ejs",{data});
+    }
+    else{
+        res.render("error.ejs");
+    }
+});
+// app.get("/ig/:username",(req,res)=>{
+//     const follower=["Anjali","Virat","Rohit","Hardik"];
+//     let {username}=req.params;
+//     res.render("instagram.ejs",{username,follower});
+// });
+
+app.get("/rolldice",(req,res)=>{
+    let diceval=Math.floor(Math.random()*6)+1;
+    res.render("rolldice.ejs",{diceval});
 })
 
 app.listen(port,()=>{
