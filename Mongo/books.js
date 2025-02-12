c2d24553c8f134e80b13ff2d457e966c0c9f3fa9
Anjali Longre <anjalilongre349@gmail.com>
2025-02-12 23:35:00 +0530

    Schema validation

---
+++ Mongo/books.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+main()
+  .then(() => {
+    console.log("Connection established successfully.");
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB:", err);
+  });
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/amazon');
+}
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+});
