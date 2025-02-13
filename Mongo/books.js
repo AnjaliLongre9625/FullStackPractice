bdf0dbe6f9df4fc4ec33b0f9258b4ad90bd9c1ac
Anjali Longre <anjalilongre349@gmail.com>
2025-02-13 15:58:23 +0530

    Schema type option

--- Mongo/books.js
+++ Mongo/books.js
@@ -9,11 +9,51 @@ main()
   });
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/amazon');
+    await mongoose.connect("mongodb://127.0.0.1:27017/amazon");
 }
 
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  age: Number,
+const bookSchema = new mongoose.Schema({
+  title: {
+    type:String,
+    required:true,
+  },
+  author: {
+    type:String,
+  },
+  price:{
+    type:Number,
+  },
+  discount:{
+    type:Number,
+    default:0
+  },
+  category:{
+    type:String,
+    enum:["fiction","nonfiction"],
+  },
 });
+
+const Book=mongoose.model("Book",bookSchema);
+
+Book.findByIdAndUpdate("67adc8146cc283b6b77ac63d0",{price:-500}).then(
+  (res)=>{
+    console.log(res);
+  })
+  .catch((err)=>{
+    console.log(err);
+  });
+  
+// let book1=new Book({
+//     title:"Marvel Comics",
+//     price:4000,
+//     category:"fiction",
+// });
+
+// book1
+//   .save() 
+//   .then((res) => {
+//     console.log("Book saved:", res);
+//   })
+//   .catch((err) => {
+//     console.log("Error:", err);
+//   });
