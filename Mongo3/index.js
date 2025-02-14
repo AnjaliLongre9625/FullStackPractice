295edb6842eb269ae487222b1b1a0ec3da85f39a
Anjali Longre <anjalilongre349@gmail.com>
2025-02-14 22:36:16 +0530

    mongo with express

--- Mongo3/index.js
+++ Mongo3/index.js
@@ -1,10 +1,37 @@
-const express=require("express");
-const app=express();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const Chat = require("./models/chat.js");
 
-app.get("/",(req,res)=>{
-    res.send("root is working");
+// Correctly setting the views directory
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+main()
+  .then(() => {
+    console.log("Connection successful");
+  })
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/whatsappDB");
+}
+
+let chat1=new Chat({
+  from:"Anjali",
+  to:"Sumiee",
+  msg:"dont irritate me ",
+  created_at:new Date()
+});
+chat1.save().then(res=>{
+  console.log(res);
 });
 
-app.listen(8080,()=>{
-  console.log("server is listening");
+app.get("/", (req, res) => {
+  res.send("Root is working!");
+});
+
+app.listen(8080, () => {
+  console.log("Server is listening on port 8080");
 });
