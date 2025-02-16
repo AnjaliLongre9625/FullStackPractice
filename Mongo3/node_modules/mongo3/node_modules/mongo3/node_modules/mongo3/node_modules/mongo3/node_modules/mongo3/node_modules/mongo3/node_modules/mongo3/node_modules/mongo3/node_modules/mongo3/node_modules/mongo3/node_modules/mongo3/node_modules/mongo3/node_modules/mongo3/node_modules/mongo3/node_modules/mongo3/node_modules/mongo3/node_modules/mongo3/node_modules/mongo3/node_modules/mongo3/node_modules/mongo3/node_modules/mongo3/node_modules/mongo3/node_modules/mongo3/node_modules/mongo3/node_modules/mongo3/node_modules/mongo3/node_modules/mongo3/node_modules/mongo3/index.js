d9fbada32b535b221325f544578207c95bb5cf8b
Anjali Longre <anjalilongre349@gmail.com>
2025-02-16 12:00:55 +0530

    mongo with express

--- Mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/index.js
+++ Mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/index.js
@@ -8,6 +8,7 @@ const Chat = require("./models/chat.js");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended:true}));
 
 main()
   .then(() => {
@@ -26,6 +27,30 @@ app.get("/chats", async (req, res) => {
   res.render("index", { chats }); // Corrected to render the EJS template
 });
 
+app.get("/chats/new", (req, res) => {
+  res.render("new.ejs");// Corrected to render the EJS template
+});
+
+//create route 
+app.post("/chats", (req, res) => {
+  let {from ,to,msg}=req.body;
+  let newChat=new Chat({
+    from:from,
+    to:to,
+    msg:msg,
+    created_at:new Date()
+  });
+  
+  newChat
+  .save()
+  .then(res=>{
+    console.log("chats was save");
+  })
+  .catch((err)=>{
+    console.log(err);
+  });
+  res.redirect("/chats");
+});
 
 
 app.get("/", (req, res) => {
