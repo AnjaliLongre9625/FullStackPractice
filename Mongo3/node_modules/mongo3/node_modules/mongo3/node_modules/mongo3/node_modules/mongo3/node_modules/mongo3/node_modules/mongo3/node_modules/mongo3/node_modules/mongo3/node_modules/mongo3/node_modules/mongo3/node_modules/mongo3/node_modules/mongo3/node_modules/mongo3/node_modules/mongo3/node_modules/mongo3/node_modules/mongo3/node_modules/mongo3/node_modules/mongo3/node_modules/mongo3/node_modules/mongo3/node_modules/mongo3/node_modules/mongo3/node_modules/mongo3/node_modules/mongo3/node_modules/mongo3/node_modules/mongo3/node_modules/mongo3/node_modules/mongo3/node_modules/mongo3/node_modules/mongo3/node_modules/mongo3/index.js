fb98b3ddec409c1a220796e4a2722cd89133497d
Anjali Longre <anjalilongre349@gmail.com>
2025-02-16 12:40:56 +0530

    mongo with express

--- Mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/index.js
+++ Mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/index.js
@@ -3,12 +3,14 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
+const methodOverride=require("method-override");
 
 // Correctly setting the views directory
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 main()
   .then(() => {
@@ -23,8 +25,8 @@ async function main() {
 // index route to get all chats
 app.get("/chats", async (req, res) => {
   let chats = await Chat.find();
-  console.log(chats);
-  res.render("index", { chats }); // Corrected to render the EJS template
+  // console.log(chats);
+  res.render("index.ejs",{chats});
 });
 
 app.get("/chats/new", (req, res) => {
@@ -40,7 +42,7 @@ app.post("/chats", (req, res) => {
     msg:msg,
     created_at:new Date()
   });
-  
+
   newChat
   .save()
   .then(res=>{
@@ -52,6 +54,25 @@ app.post("/chats", (req, res) => {
   res.redirect("/chats");
 });
 
+//Edit Route
+app.get("/chats/:id/edit", async(req, res) => {
+  let {id}=req.params;
+  let chat= await Chat.findById(id);
+  res.render("edit.ejs",{chat});
+});
+
+//UPDATE ROUTE
+app.put("/chats/:id", async(req, res) => {
+  let {id}=req.params;
+  let {msg:newMsg}=req.body;
+  let updatedChat= await Chat.findByIdAndUpdate(
+    id,
+    {msg:newMsg},
+    {runValidators:true,new:true}
+  );
+  console.log(updatedChat);
+  res.redirect("/chats");
+});
 
 app.get("/", (req, res) => {
   res.send("Root is working!");
--- Mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/tempCodeRunnerFile.js
+++ Mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/tempCodeRunnerFile.js
@@ -7,6 +7,8 @@ const Chat = require("./models/chat.js");
 // Correctly setting the views directory
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended:true}));
 
 main()
   .then(() => {
@@ -20,10 +22,42 @@ async function main() {
 
 // index route to get all chats
 app.get("/chats", async (req, res) => {
-    let chats = await Chat.find(); // Correct query to get all chats
-    console.log(chats);
-    res.send("index.ejs",{chats}); // Send chats as JSON response
+  let chats = await Chat.find();
+  console.log(chats);
+  res.render("index", { chats }); // Corrected to render the EJS template
+});
+
+app.get("/chats/new", (req, res) => {
+  res.render("new.ejs");// Corrected to render the EJS template
+});
+
+//create route 
+app.post("/chats", (req, res) => {
+  let {from ,to,msg}=req.body;
+  let newChat=new Chat({
+    from:from,
+    to:to,
+    msg:msg,
+    created_at:new Date()
   });
+
+  newChat
+  .save()
+  .then(res=>{
+    console.log("chats was save");
+  })
+  .catch((err)=>{
+    console.log(err);
+  });
+  res.redirect("/chats");
+});
+//Edit Route
+app.get("/chats/:id/edit", async(req, res) => {
+  let {id}=req.params;
+  let chat= await Chat.findById(id);
+  res.render("edit.ejs",{chat});
+});
+
 
 app.get("/", (req, res) => {
   res.send("Root is working!");
