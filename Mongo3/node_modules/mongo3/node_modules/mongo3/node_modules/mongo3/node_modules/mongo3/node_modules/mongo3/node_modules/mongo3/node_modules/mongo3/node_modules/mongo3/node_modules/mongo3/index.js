6f4333f57f6291eba9ddac4b87a8185f9e40db52
Anjali Longre <anjalilongre349@gmail.com>
2025-02-20 14:55:59 +0530

    Airbnb

--- Mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/index.js
+++ Mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/node_modules/mongo3/index.js
@@ -74,6 +74,15 @@ app.put("/chats/:id", async(req, res) => {
   res.redirect("/chats");
 });
 
+//destroy route
+app.get("/chats/:id", async(req, res) => {
+  let {id}=req.params;
+  let deletedChat=await chat.findByIdAndDelete(id);
+  console.log(deletedChat);
+  res.redirect("/chats");
+});
+
+
 app.get("/", (req, res) => {
   res.send("Root is working!");
 });
