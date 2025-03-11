42b461af35a1503654e469a0cafebdaf7ae4e510
Anjali Longre <anjalilongre349@gmail.com>
2025-03-11 13:24:15 +0530

    using populate

--- Relationships/Models/customer.js
+++ Relationships/Models/customer.js
@@ -18,14 +18,39 @@ const customerSchema=new Schema({
     name:String,
     orders:[
         {
-            
+           type:Schema.Types.ObjectId,
+           ref:"Order"
         }
     ]
-})
+});
 
 const Order=mongoose.model("Order",orderSchema);
+const Customer=mongoose.model("Customer",customerSchema);
+
+const findCustomer=async()=>{
+//    let cust1=new Customer({
+//       name:"Anjali",
+//     });
+
+//     let order1=await Order.findOne({item:"Chips"});
+//     let order2=await Order.findOne({item:"Chocolate"});
+    
+//     cust1.orders.push(order1);
+//     cust1.orders.push(order2);
+
+//     let result=await cust1.save();
+//     console.log(result);
+  let result=await Customer.find();
+  console.log(result);
+};
+findCustomer();
 
 // const addOrders=async () => {
 //     let res=await Order.insertMany([
 //         {item:"Samose",price:12},
-//         {item:"Chips",price
+//         {item:"Chips",price:10},
+//         {item:"Chocolate",price:40},
+//         ]);
+//         console.log(res);
+// };
+// addOrders();
