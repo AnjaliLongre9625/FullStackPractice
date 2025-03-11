2fb15de26bb3922c9db570e78a20b77eedf315b3
Anjali Longre <anjalilongre349@gmail.com>
2025-03-11 13:07:50 +0530

    one to many

--- Relationships/Models/user.js
+++ Relationships/Models/user.js
@@ -9,4 +9,31 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/relationDemo');
 }
 
-const userSchema=new Sche
+const userSchema=new Schema({
+    username:String,
+    addresses:[
+        {
+            location:String,
+            city:String,
+        },
+    ],
+});
+const User=mongoose.model("User",userSchema);
+
+const addUsers = async() =>{
+   let user1=new User({
+     username:"Anjali",
+     addresses:[
+        {
+        _id:false,
+        location:"221b Baker street",
+        city:"london",
+     },
+    ],
+   });
+   
+   user1.addresses.push({location:"p32 WallStreet",city:"london"});
+   let result=await user1.save();
+   console.log(result);
+}
+addUsers(
