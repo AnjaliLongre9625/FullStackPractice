f6f542faf54daf85e9e56699198d9bd757381273
Anjali Longre <anjalilongre349@gmail.com>
2025-03-11 12:48:44 +0530

    sql

--- Relationships/Models/user.js
+++ Relationships/Models/user.js
@@ -1,1 +1,12 @@
-const mongoose =
+const mongoose = require("mongoose");
+const {Schema}=mongoose;
+
+main()
+.then(() => console.log("connection successfull"))
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/relationDemo');
+}
+
+const userSchema=new Sche
